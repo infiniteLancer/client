f6c1a57a40427ec22fd3dc1a234c2771e00a7b7d
edwinsatya <edwinsatyayudistira@gmail.com>
2019-11-07 16:50:27 +0700

    buefy import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-
-
 import Vue from 'vue';
 import App from './App.vue';
+//buefy
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
-
