401af6f6dc47f93b942634daef524e420969620f
evans <karlinevans333@gmail.com>
2019-11-07 16:08:27 +0700

    template

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="alfred">
+      alfred
+    </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello world',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.alfred{
+  background-color: black;
+  color: white;
+}
+</style>
+
