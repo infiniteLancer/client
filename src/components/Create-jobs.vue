2cca3c442828196ffb9d7941b5d3f2667927f880
dwitamaalfred <dwitama.alfred@gmail.com>
2019-11-08 11:14:46 +0700

    update create-jobs

--- src/components/Create-jobs.vue
+++ src/components/Create-jobs.vue
@@ -1,26 +1,16 @@
 <template>
    <form action="" class="box" style="padding:5rem;display:flex;flex-direction:column;justify-content:center;">
         <div class="field">
-        <label for="" class="label">Title</label>
+        <label for="" class="label">Name</label>
         <div class="control has-icons-left">
-            <input v-model="title" type="username" placeholder="e.g. bobsmith" class="input">
+            <input v-model="name" type="username" placeholder="Projects Name" class="input">
             <span class="icon is-small is-left">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-tasks"></i>
             </span>
         </div>
         </div>
 
-    <div class="field">
-        <label for="" class="label">Email</label>
-        <div class="control has-icons-left">
-            <input v-model="email" type="text" placeholder="e.g. bobsmith@gmail.com" class="input">
-            <span class="icon is-small is-left">
-            <i class="fa fa-envelope"></i>
-            </span>
-        </div>
-        </div>
-
-        <b-field label="Message"
+    <b-field label="Description"
         :label-position="Description">
         <b-input v-model="description" maxlength="200" type="textarea" placeholder="Describe your projects details" ></b-input>
     </b-field>
@@ -59,18 +49,18 @@
                         </button>
                     </span>
                 </div>
-                <template>
-                    <section>
-                        <b-field label="what is your speciality">
-                            <b-taginput
-                                v-model="tags"
-                                ellipsis
-                                icon="label"
-                                placeholder="Add a tag">
-                            </b-taginput>
-                        </b-field>
-                    </section>
-                </template>
+        <template>
+            <section>
+                <b-field label="Requirements">
+                    <b-taginput
+                        v-model="tags"
+                        ellipsis
+                        icon="label"
+                        placeholder="ex : programmer">
+                    </b-taginput>
+                </b-field>
+            </section>
+        </template>
             </section>
         </div>
         </div>
@@ -78,22 +68,21 @@
         <div class="field">
         <label for="" class="label">Deadline</label>
         <div class="control has-icons-left">
-            <input v-model="deadline" type="text" placeholder="yyyy-mm-dd" class="input">
+            <input v-model="deadline" type="date" placeholder="yyyy-mm-dd" class="input">
             <span class="icon is-small is-left">
-            <i class="fa fa-envelope"></i>
+            <i class="fas fa-stopwatch"></i>
             </span>
         </div>
-        </div>
-
 
         <div class="field">
         <label for="" class="label">Phone</label>
         <div class="control has-icons-left">
-            <input v-model="phone" type="phone" placeholder="+62 " class="input">
+            <input v-model="phone" type="text" placeholder="+62 " class="input">
             <span class="icon is-small is-left">
             <i class="fas fa-mobile-alt"></i>
             </span>
         </div>
+
         </div>
 
         <div class="field" style="display:flex; justify-content:space-between; margin-right:100px; margin-left:100px">
